38b30045f2d7da3fc28d16e9f00cbb31c4fa5c71
王艺 <wy0329@foxmail.com>
2022-09-05 00:18:20 +0800

    flood fill 相关问题

---
+++ leetcode/editor/cn/Code200NumberOfIslands.java
@@ -0,0 +1,71 @@
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。 
+//
+// 此外，你可以假设该网格的四条边均被水包围。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 300 
+// grid[i][j] 的值为 '0' 或 '1' 
+// 
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 👍 1882 👎 0
+
+
+package cn;
+
+/**
+ * @author cloaks
+ * @questionId 200
+ * @title 岛屿数量
+ * @titleSlug number-of-islands
+ * @date 2022-09-05 00:13:06
+ */
+public class Code200NumberOfIslands {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        Solution solution = new Code200NumberOfIslands().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numIslands(char[][] grid) {
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
---
+++ leetcode/editor/cn/Code827MakingALargeIsland.java
@@ -0,0 +1,68 @@
+//给你一个大小为 n x n 二进制矩阵 grid 。最多 只能将一格 0 变成 1 。 
+//
+// 返回执行此操作后，grid 中最大的岛屿面积是多少？ 
+//
+// 岛屿 由一组上、下、左、右四个方向相连的 1 形成。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: grid = [[1, 0], [0, 1]]
+//输出: 3
+//解释: 将一格0变成1，最终连通两个小岛得到面积为 3 的岛屿。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: grid = [[1, 1], [1, 0]]
+//输出: 4
+//解释: 将一格0变成1，岛屿的面积扩大为 4。 
+//
+// 示例 3: 
+//
+// 
+//输入: grid = [[1, 1], [1, 1]]
+//输出: 4
+//解释: 没有0可以让我们变成1，面积依然为 4。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == grid.length 
+// n == grid[i].length 
+// 1 <= n <= 500 
+// grid[i][j] 为 0 或 1 
+// 
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 👍 180 👎 0
+
+
+package cn;
+/**
+ * @author cloaks
+ * @questionId 827
+ * @title 最大人工岛
+ * @titleSlug making-a-large-island
+ * @date 2022-09-05 00:16:50
+ */
+public class Code827MakingALargeIsland{
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        Solution solution = new Code827MakingALargeIsland().new Solution();
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int largestIsland(int[][] grid) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
